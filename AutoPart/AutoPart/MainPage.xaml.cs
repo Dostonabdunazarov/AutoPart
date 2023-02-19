aee1e77062176292106189e7136e6888fe4c898e
DostonAbdunazarov <abdunazarov.doston@mail.ru>
2023-02-19 19:23:49 +0500

    add filePicker functional

--- AutoPart/AutoPart/MainPage.xaml.cs
+++ AutoPart/AutoPart/MainPage.xaml.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using Syncfusion.XlsIO;
+using Xamarin.Essentials;
+using System.IO;
 
 namespace AutoPart
 {
@@ -14,5 +13,137 @@ namespace AutoPart
         {
             InitializeComponent();
         }
+        async void PickExcelFilesClicked(object sender, EventArgs e)
+        {
+            var allowedUTIs = new string[] {
+                "com.microsoft.excel.xls",
+                "com.microsoft.excel.xlsx",
+                "org.openxmlformats.wordprocessingml.document",
+                "*/*",
+                "*",
+                ".*",
+                ".xlsx",
+                "*.*"
+            };
+            var customFileType = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+            {
+                { DevicePlatform.Android, allowedUTIs },
+                { DevicePlatform.iOS, allowedUTIs },
+            });
+
+            var Files = await FilePicker.PickMultipleAsync(new PickOptions
+            {
+                FileTypes = customFileType,
+                PickerTitle = "Pick an excel"
+            });
+            var FilePaths = new List<string>();
+            var FileNames = new List<string>();
+            if (Files != null)
+            {
+                foreach (var file in Files)
+                {
+                    FilePaths.Add(file.FullPath);
+                    FileNames.Add(file.FileName);
+                }
+            }
+            
+            var LabelsList = new List<Label[,]>();
+            var RowsCountList = new List<int>();
+            var nameCol = new List<int>();
+            var priceCol = new List<int>();
+            for (int k = 0; k < FilePaths.Count; k++)
+            {
+                ExcelEngine excelEngine = new ExcelEngine();
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Xlsx;
+                FileStream fileStream = new FileStream(FilePaths[k], FileMode.Open);
+                IWorkbook workbook = application.Workbooks.Open(fileStream);
+                IWorksheet worksheet = workbook.Worksheets["Лист1"];
+                int rowCount = worksheet.Rows.Length;
+                int colCount = worksheet.Columns.Length;
+
+                for (int i = 0; i < rowCount; i++)
+                {
+                    for (int j = 0; j < colCount; j++)
+                    {
+                        if (worksheet.GetValueRowCol(i, j).ToString() == "#")
+                        {
+                            nameCol.Add(j);
+                        }
+                        if (worksheet.GetValueRowCol(i, j).ToString() == "$")
+                        {
+                            priceCol.Add(j);
+                        }
+                    }
+                }
+
+                RowsCountList.Add(rowCount);
+                var labels = new Label[rowCount, colCount];
+                for (int i = 1; i <= rowCount; i++)
+                {
+                    if (!String.IsNullOrEmpty(worksheet.GetValueRowCol(i, nameCol[k])?.ToString()) && !String.IsNullOrEmpty(worksheet.GetValueRowCol(i, priceCol[k])?.ToString()))
+                    {
+                        labels[i - 1, nameCol[k]] = new Label { Text = worksheet.GetValueRowCol(i, nameCol[k]).ToString(), FontFamily = "Arial", };
+                        labels[i - 1, priceCol[k]] = new Label { Text = worksheet.GetValueRowCol(i, priceCol[k]).ToString(), FontFamily = "Arial", };
+                    }
+                }
+                LabelsList.Add(labels);
+                fileStream.Close();
+                workbook.Close();
+                excelEngine.Dispose();
+            }
+            
+            _ = Navigation.PushAsync(new MainTabbedPage(LabelsList, RowsCountList, nameCol, priceCol, FileNames));
+        }
     }
 }
+
+
+//int nameCol = 0;
+//int priceCol = 0;
+//int rowCount = 0;
+//int colCount = 0;
+//foreach (var file in FilePaths)
+//{
+//    fileStream = new FileStream(file, FileMode.Open);
+//    IWorkbook workbook = application.Workbooks.Open(fileStream);
+//    IWorksheet worksheet = workbook.Worksheets["Лист1"];
+//    rowCount = worksheet.Rows.Length;
+//    colCount = worksheet.Columns.Length;
+
+//    for (int i = 0; i < rowCount; i++)
+//    {
+//        for (int j = 0; j < colCount; j++)
+//        {
+//            if (worksheet.GetValueRowCol(i, j).ToString() == "#")
+//            {
+//                nameCol = j;
+//            }
+//            else if (worksheet.GetValueRowCol(i, j).ToString() == "$")
+//            {
+//                priceCol = j;
+//                break;
+//            }
+//        }
+//    }
+
+//    for (int i = 1; i <= rowCount; i++)
+//    {
+//        for (int j = 1; j <= colCount; j++)
+//        {
+
+//            if (!String.IsNullOrEmpty(worksheet.GetValueRowCol(i, j).ToString()) && rowCount != 0 && nameCol != 0)
+//            {
+//                var label = new Label()
+//                {
+//                    Text = worksheet.GetValueRowCol(i, nameCol).ToString() + " - " + worksheet.GetValueRowCol(i, priceCol).ToString(),
+//                };
+//                Labels.Add(label);
+//            }
+//                //Names.Add(worksheet.GetValueRowCol(i, nameCol).ToString() + " - " + worksheet.GetValueRowCol(i, priceCol).ToString());
+//        }
+//    }
+//}
+
+
+//_ = Navigation.PushAsync(new MainTabbedPage(LabelsList, RowsCountList, ColumnCountList, FileNames));
---
+++ AutoPart/AutoPart/MainTabbedPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AutoPart
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainTabbedPage : TabbedPage
+    {
+        public List<Label[,]> LabelsList { get; set; }
+        public List<int> RowCounts { get; set; }
+        public List<string> FileNamesList { get; set; }
+        public List<Entry> Entries { get; set; }
+        public List<Label> Labels { get; set; }
+        public List<string> NameList { get; set; }
+        public List<int> NameCol { get; set; }
+        public List<int> PriceCol { get; set; }
+
+        public MainTabbedPage(List<Label[,]> labels, List<int> rowCounts, List<int> nameCol, List<int> priceCol, List<string> fileNames)
+        {
+            InitializeComponent();
+            Labels = new List<Label>();
+            LabelsList = labels;
+            RowCounts = rowCounts;
+            NameCol = nameCol;
+            PriceCol = priceCol;
+            FileNamesList = fileNames;
+            Entries = new List<Entry>();
+            CreateComponents();
+        }
+        void GetResultSearch(object sender, TextChangedEventArgs e)
+        {
+            //
+        }
+        void CreateComponents(Grid grid = null)
+        {
+            for (int i = 0; i < FileNamesList.Count; i++)
+            {
+                var contentPage = new ContentPage() { Title = FileNamesList[i] };
+                var stackLayout = new StackLayout();
+                var scrollView = new ScrollView() { HorizontalScrollBarVisibility = ScrollBarVisibility.Always, VerticalOptions = LayoutOptions.FillAndExpand };
+                var entrySearch = new Entry() { Placeholder = "Search" };
+                entrySearch.TextChanged += GetResultSearch;
+                Grid GridContent = new Grid();
+                Entries.Add(entrySearch);
+                GridContent.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Auto) });
+                GridContent.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Auto) });
+                GridContent.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(200, GridUnitType.Absolute) });
+                GridContent.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(100, GridUnitType.Auto) });
+                for (int k = 0; k < RowCounts[i]; k++)
+                {
+                    GridContent.Children.Add(new Label { Text = (k + 1).ToString() }, 0, k);
+                    if (LabelsList[i][k, NameCol[i]] != null && NameCol[i] != 0)
+                    {
+                        GridContent.Children.Add(LabelsList[i][k, NameCol[i]], NameCol[i], k);
+                    }
+                    if (LabelsList[i][k, PriceCol[i]] != null && PriceCol[i] != 0)
+                    {
+                        GridContent.Children.Add(LabelsList[i][k, PriceCol[i]], PriceCol[i], k);
+                    }
+                }
+                scrollView.Content = GridContent;
+                stackLayout.Children.Add(scrollView);
+                stackLayout.Children.Add(entrySearch);
+                contentPage.Content = stackLayout;
+                MyTabbedPage.Children.Add(contentPage);
+            }
+        }
+    }
+}
